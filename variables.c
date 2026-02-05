12391f0e29c577c73ef12b559589606458e57e02
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-02-05 20:28:16 +0300

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+  int age = 32;
+  printf("he is %d\n", age);
+  
+  age = 65;
+  printf("his father is %d\n", age);
+
+  float gpa = 3.0;
+  printf("she got a %.2f\n", gpa):
+
+  double pi = 3.1214;
+  printf("pi is %.4lf\n", pi);
+
+  double exp = 2.4e6;
+  printf("the value was %.2lf\n", exp);  
+
+  char block = 'A';
+  printf("block: %c\n", block);
+  
+  return 0;
+}
